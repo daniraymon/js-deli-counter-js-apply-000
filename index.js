13aefb5d756a9f9d6ebadfc932a74b6cd0171490
Daniel Raymon <daniraymon@gmail.com>
2017-09-09 01:24:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function takeANumber(katzDeliLine, customer) {
-  var katzDeli = [];
+var katzDeli = [];
+
+function takeANumber(katzDeli, customer) {
+  
   var position = katzDeliLine.length + 1;
   return "Welcome, " + customer + ". You are number " + position + " in line.";
 }
